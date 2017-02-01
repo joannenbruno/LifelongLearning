6872bb2e86f3f290b79d585356f9fb4d32478b88
Joanne Bruno <jnbruno2015@gmail.com>
2017-02-01 14:56:22 -0600

    init commit modern web ng2 trng

--- AngularJS/ModernWebTrng/app/app.component.ts
+++ AngularJS/ModernWebTrng/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: '<h1>My name is Joanne. And I am Cool.</h1>'
 })
 export class AppComponent { }
